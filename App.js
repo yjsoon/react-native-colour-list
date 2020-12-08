412072292050167c2d3b9b12cc3245030f70a627
YJ Soon <yjsoon@tinkertanker.com>
2020-12-08 11:23:33 +0800

    Add colour button now works

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 import { FlatList } from "react-native-gesture-handler";
 
-// const COLOURS = [
-//   { red: 255, green: 128, blue: 0, id: "0" },
-//   { red: 0, green: 128, blue: 255, id: "1" },
-//   { red: 128, green: 0, blue: 255, id: "2" },
-// ];
-
 function HomeScreen() {
-  const [colorArray, setColorArray] = useState([
-    { red: 255, green: 128, blue: 128, id: "0" },
-    { red: 0, green: 128, blue: 255, id: "1" },
-    { red: 128, green: 0, blue: 255, id: "2" },
-  ]);
+  const [colorArray, setColorArray] = useState([]);
 
   function renderItem({ item }) {
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
   }
 
+  function addColor() {
+    let newColor = {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: colorArray.length.toString(),
+    };
+    setColorArray([...colorArray, newColor]);
+  }
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "red" }}>Add Colour</Text>
+      </TouchableOpacity>
       <FlatList
         style={{ width: "100%" }}
         data={colorArray}
